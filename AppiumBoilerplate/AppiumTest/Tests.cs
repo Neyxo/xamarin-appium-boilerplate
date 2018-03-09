fbb20742ba226ca0525903ef04fde31f265bede7
Jean-Michel Carrel <neyxoproductions@gmail.com>
2018-03-09 16:22:39 +0100

    Appium test set up

--- AppiumBoilerplate/AppiumTest/Tests.cs
+++ AppiumBoilerplate/AppiumTest/Tests.cs
@@ -13,9 +13,22 @@ using OpenQA.Selenium.Appium.Android;
 [TestFixture()]
 public class Tests
 {
+    public AndroidDriver<AppiumWebElement> driver;
+
     [SetUp]
     public void SetUp()
     {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        // Set device capabilities
+        capabilities.SetCapability("platformName", "Android");
+        capabilities.SetCapability("platformVersion", "7.0");
+        capabilities.SetCapability("deviceName", "mydevice");
+        // Set application capabilities
+        capabilities.SetCapability("appPackage", "com.companyname.AppiumBoilerplate");
+        capabilities.SetCapability("appActivity", "md5d297de0b36d5377ccc45b847c53a985d.MainActivity");
+
+        driver = new AndroidDriver<AppiumWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities, TimeSpan.FromSeconds(180));
+        driver.LaunchApp();
     }
 
 
@@ -24,5 +37,6 @@ public class Tests
     [TearDown]
     public void End()
     {
+        driver.CloseApp();
     }
 }
